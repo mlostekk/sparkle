2b665c9e9969c8368b6d3c49efe8eda9b80843b8
Martin 'Arkardiusz' Mlostek <mlostek@gmail.com>
2017-12-17 13:14:16 +0100

    added empty template project

---
+++ android/app/src/main/java/com/nomad5/sparkle/QuestionsActivity.kt
@@ -0,0 +1,12 @@
+package com.nomad5.sparkle
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class QuestionsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_questions)
+    }
+}
